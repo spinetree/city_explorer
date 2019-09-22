a0bb7444d414ed911201aab3e461e6c799958b02
spinetree <steve@spinetree.org>
2019-09-21 19:35:59 -0700

    talking to google api

--- server.js
+++ server.js
@@ -2,19 +2,20 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
+// globals
+
+let currentLocation;
+
 
 // ---------- Dependencies
 
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-const superagent = require('superagent');
+const superAgent = require('superagent');
 
 // why different syntax here? This is a method dotenv gives us access to?
 require('dotenv').config();
-
-// ----- end dependencies
-
 
 
 // ---------- run all the big picture things
@@ -34,15 +35,18 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}, let's party`) )
 
 // ---------- routes
 
+app.get('/weather', () => console.log('hit /weather') )
+app.get('/location', (request,response) => {
+  console.log('hit /location');
+  getLocationGoogle(request, response);
+}
+)
+
 app.get('*', () => console.log('default route here') )
 
-// ---------- end routes
+// ---------- imports
 
-
-
-
-// ---------- Constructor Functions
-
+// Constructor Functions
 // do not fully grok this second line which seems to only be for constructor functions, gotten from https://stackabuse.com/how-to-use-module-exports-in-node-js/ but seems to work
 
 let locations = require('./location.js');
@@ -54,14 +58,42 @@ let Weather = weathers.Weather;
 let events = require('./event.js');
 let Event = events.Event;
 
-// ----- end constructor functions
+
+// ---------- get location from google
+
+function getLocationGoogle(request, response) {
+
+  let query = request.query.data;
+  let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  console.log(`geocode url: ${geocodeUrl}`);
+
+  superAgent.get(geocodeUrl)
+    .then(googleResponse => {
+      console.log(googleResponse);
+      // run through constructor function
+      response.send(currentLocation);
+    })
+    .catch(error => {
+      errorHandler(error,request, response)
+    }
+    )
+}
 
 
-// error handler
 
 
 
-// tests
-Location('derpQuery','derpFormatted','234234','q34');
-Weather({summary:'Example daily summary', time: 8287888399});
-Event({url:'someURL', name: {text:'thisname'}, start: {local:[3,4,5,2,24,5]} });
+
+// ---------- error handler
+
+function errorHandler(error, reqest, response) {
+  console.error(error);
+  response.status(500).send('errorHandler says: Something went wrong');
+}
+
+
+// --------------- tests
+
+// Location('derpQuery','derpFormatted','234234','q34');
+// Weather({summary:'Example daily summary', time: 8287888399});
+// Event({url:'someURL', name: {text:'thisname'}, start: {local:[3,4,5,2,24,5]} });
